394d20e88b07490c757ec5e7118ae9920b93e1c8
barek <bareketatia@gmail.com>
2022-06-02 12:28:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+ */
+
+public class MainActivity extends AppCompatActivity {
+   /*
+    private TextView instructions;
+    private TextView firstNameTitle;
+    private EditText firstNameText;
+    private TextView lastNameTitle;
+    private EditText lastNameText;
+    private TextView age;
+    private EditText ageText;
+    private TextView genderTitle;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private TextView bodyStructureTitle;
+    private RadioButton smallButton;
+    private RadioButton mediumButton;
+    private RadioButton LargeButton;
+    private TextView heightTitle;
+    private SeekBar heightSlider;
+    private TextView weightTitle;
+    private EditText weightText;
+    private TextView bmiTitle;
+    private TextView bmiResult;
+    private TextView idealWeightTitle;
+    private TextView idealWeightResult;
+    private Button submitButton;
+    private Button clearButton;
+
+    */
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
